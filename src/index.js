dea9689bb0775add6bf36bfa8936162960648982
awatersny <awatersny@gmail.com>
2025-03-14 13:46:53 -0400

    Add event listeners to each form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ const errorDisplay = document.getElementById("errorDisplay")
 const loginForm = document.getElementById("login")
 const registerForm = document.getElementById("registration")
 
+loginForm.addEventListener("submit", loginFormCtrl)
+registerForm.addEventListener("submit", registerFormCtrl)
+
+function loginFormCtrl(evt) {
+  evt.preventDefault()
+  console.log("login")
+}
+
+function registerFormCtrl(evt) {
+  evt.preventDefault()
+  console.log("register")
+}
+
 function displayError(error) {
   errorDisplay.textContent = error
   errorDisplay.style.display = "flex"
