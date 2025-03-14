236216c4211fb04ae1ea0544a8948d51addbf519
awatersny <awatersny@gmail.com>
2025-03-14 14:01:08 -0400

    Cache text input fields

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const errorDisplay = document.getElementById("errorDisplay")
-const loginForm = document.getElementById("login")
+
 const registerForm = document.getElementById("registration")
+const registerUsername = registerForm.childNodes[1]
+const registerEmail = registerForm.childNodes[6]
+const registerPassword = registerForm.childNodes[11]
+const registerConfirmP = registerForm.childNodes[16]
+
+const loginForm = document.getElementById("login")
+
+console.log(loginForm.childNodes)
+const loginUsername = loginForm.childNodes[1]
+const loginPassword = loginForm.childNodes[6]
 
 loginForm.addEventListener("submit", loginFormCtrl)
 registerForm.addEventListener("submit", registerFormCtrl)
