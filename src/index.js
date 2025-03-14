476a7026064590d071963c4ebcfffbb40d667251
awatersny <awatersny@gmail.com>
2025-03-14 13:41:27 -0400

    Cache each form individually

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const errorDisplay = document.querySelector("#errorDisplay")
-const displayError = (error) => {
+const errorDisplay = document.getElementById("errorDisplay")
+const loginForm = document.getElementById("login")
+const registerForm = document.getElementById("registration")
+
+function displayError(error) {
   errorDisplay.textContent = error
   errorDisplay.style.display = "flex"
 }
 
+function removeError() {
+  errorDisplay.textContent = ""
+  errorDisplay.style.display = "none"
+}
