14831d8d6fb8462af9ef854814843543a09a340b
awatersny <awatersny@gmail.com>
2025-03-15 14:51:07 -0400

    Ensure all login fields are filled out

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ const errorDisplay = document.getElementById("errorDisplay")
 
 const registerForm = document.getElementById("registration")
 
-const register = {
+const loginForm = document.getElementById("login")
+
+const registerFields = {
   username: registerForm.childNodes[1],
   email: registerForm.childNodes[6],
   password: registerForm.childNodes[11],
   confirmP: registerForm.childNodes[16]
 }
 
-const loginForm = document.getElementById("login")
-
-const loginUsername = loginForm.childNodes[1]
-const loginPassword = loginForm.childNodes[6]
+const loginFields = {
+  username: loginForm.childNodes[1],
+  password: loginForm.childNodes[6]
+}
 
 const emailValidation = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/
 // console.log(emailValidation.test("awatersny@gmail.com"))
@@ -25,64 +27,64 @@ loginForm.addEventListener("click", removeError)
 function registerFormSubmitCtrl(evt) {
   evt.preventDefault()
   // The username cannot be blank.
-  for(field in register) {
-    if(register[field].value === "") {
+  for(field in registerFields) {
+    if(registerFields[field].value === "") {
       displayError("All fields must be filled out")
       return
     }
   }
   // The username must be at least four characters long.
-  if(register.username.value.length < 5) {
+  if(registerFields.username.value.length < 5) {
     displayError("Username must be at least four characters long.")
     return
   }
   // The username must contain at least two unique characters.
-  if(/\w{1,}/.test(register.username.value)) {
-    if(!/\d{1,}/.test(register.username.value)) {
+  if(/\w{1,}/.test(registerFields.username.value)) {
+    if(!/\d{1,}/.test(registerFields.username.value)) {
       displayError("Username must contain at least two unique characters.")
       return
     }
-    if(!/[A-Za-z]/.test(register.username.value)) {
+    if(!/[A-Za-z]/.test(registerFields.username.value)) {
       displayError("Username must contain at least two unique characters.")
       return
     }
   }
   // The username cannot contain any special characters or whitespace.
-  if(/\s{1,}/.test(register.username.value) || /\W{1,}/.test(register.username.value)) {
+  if(/\s{1,}/.test(registerFields.username.value) || /\W{1,}/.test(registerFields.username.value)) {
     displayError("Username cannot contain any special characters or whitespace.")
     return
   }
   // The email must be a valid email address.
-  if(!emailValidation.test(register.email.value)) {
+  if(!emailValidation.test(registerFields.email.value)) {
     displayError("Email address format is invalid.")
     return
   }
   // The email must not be from the domain "example.com."
-  if(/@example.com/.test(register.email.value)) {
+  if(/@example.com/.test(registerFields.email.value)) {
     displayError(`Invalid domain.  Please use a domain other than "example.com"`)
     return
   }
   // Passwords must be at least 12 characters long.
-  if(register.password.value.length < 13) {
+  if(registerFields.password.value.length < 13) {
     displayError("Passwords must be at least 12 characters long.")
     return
   }
   // Passwords must have at least one uppercase and one lowercase letter.
-  if(/@/.test(register.password.value)) {
+  if(/@/.test(registerFields.password.value)) {
     displayError(`Invalid domain.  Please use a domain other than "example.com"`)
     return
   }
   // Passwords cannot contain the username.
-  if(register.password.value === register.username.value){
+  if(registerFields.password.value === registerFields.username.value){
     displayError("Password should not be the same as your username.")
     return
   }
   // Passwords must contain at least one number.
   // Passwords must contain at least one special character.
   // Passwords cannot contain the word "password" (uppercase, lowercase, or mixed).
-  
+
   // Both passwords must match.
-  if(register.confirmP.value !== register.password.value){
+  if(registerFields.confirmP.value !== registerFields.password.value){
     displayError("Passwords do not match.")
     return
   }
@@ -90,6 +92,13 @@ function registerFormSubmitCtrl(evt) {
 
 function loginFormSubmitCtrl(evt) {
   evt.preventDefault()
+
+  for(field in loginFields) {
+    if(loginFields[field].value === "") {
+      displayError("All fields must be filled out")
+      return
+    }
+  }
 }
 
 function displayError(error) {
